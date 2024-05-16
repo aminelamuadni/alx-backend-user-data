ad74f52d05b20655484e0bde60be891cdd4d7dfd
Amine Lamuadni <W7equwE5@gmail.com>
2024-05-16 21:54:18 +0100

    Update filtered_logger.py file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,6 @@
 This module provides functionality to obfuscate specified fields in log
 messages.
 """
-
 import re
 import logging
 import os
@@ -20,13 +19,13 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     Obfuscates specified fields in a log message.
 
     Arguments:
-    fields -- list of fields to obfuscate
-    redaction -- replacement value for the fields
-    message -- log message to process
-    separator -- character that separates fields in the log message
+        fields -- list of fields to obfuscate
+        redaction -- replacement value for the fields
+        message -- log message to process
+        separator -- character that separates fields in the log message
 
     Returns:
-    A log message with specified fields obfuscated.
+        A log message with specified fields obfuscated.
     """
     for field in fields:
         message = re.sub(f"{field}=.*?{separator}",
@@ -57,8 +56,8 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """
-    Connects to the MySQL database using environment variables and returns
-    the connection object.
+    Connects to the MySQL database using environment variables and returns the
+    connection object.
 
     Returns:
         MySQLConnection: A connection to the database.
