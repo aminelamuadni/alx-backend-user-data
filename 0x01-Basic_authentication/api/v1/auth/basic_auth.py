b3ce9c0a6af5349e77e82ecbf8584053258da840
Amine Lamuadni <W7equwE5@gmail.com>
2024-05-22 00:04:54 +0100

    Add basic_auth.py file

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+BasicAuth module for managing basic HTTP authentication in a Flask application.
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class for handling basic HTTP authentication.
+    """
+    pass
