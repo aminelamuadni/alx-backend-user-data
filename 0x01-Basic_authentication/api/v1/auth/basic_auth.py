bd2353f69db269251a845b9fcf6091c12198f16d
Amine Lamuadni <W7equwE5@gmail.com>
2024-05-22 00:21:30 +0100

    Add basic_auth.py file

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,8 @@ BasicAuth module for managing basic HTTP authentication in a Flask application.
 
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -75,3 +77,30 @@ class BasicAuth(Auth):
             return None, None
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """
+        Retrieves a User instance based on user email and password.
+
+        Args:
+            user_email (str): The user's email address.
+            user_pwd (str): The user's password.
+
+        Returns:
+            UserType: The User instance if found and password is correct;
+                      otherwise, None.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+        user = users[0]  # Assume the first returned user is the one we want
+
+        if user and user.is_valid_password(user_pwd):
+            return user
+        return None
