bf79d1cf0ce2205e7dc64d80325eb094754527bf
Amine Lamuadni <W7equwE5@gmail.com>
2024-05-23 23:55:30 +0100

    Add session_auth.py file

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Session authentication management for a Flask application.
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    SessionAuth class for handling session-based authentication.
+    """
+    pass
