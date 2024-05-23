11a9fe73c920a1c46567eff671db83f2ec801ae5
Amine Lamuadni <W7equwE5@gmail.com>
2024-05-24 00:24:33 +0100

    Add session_auth.py file

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Handles session authentication for the API. This module defines routes for
+managing session login, allowing users to establish a session via a POST
+request.
+"""
+
+from flask import request, jsonify, abort, make_response
+from api.v1.views import app_views
+from models.user import User
+import os
+from api.v1.app import auth
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """
+    Handles the POST request for user login. It validates the user's email and
+    password, and if correct, sets a session ID cookie.
+    """
+    email = request.form.get('email')
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+
+    password = request.form.get('password')
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    users = User.search({'email': email})
+    if not users:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    user = users[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    session_id = auth.create_session(user.id)
+    response = make_response(user.to_json())
+    session_name = os.getenv('SESSION_NAME', 'session_id')
+    response.set_cookie(session_name, session_id)
+
+    return response
