b8d6a8d9df7e8d9aa80fed7244bd38cc15fc450e
Amine Lamuadni <W7equwE5@gmail.com>
2024-05-31 05:19:34 +0100

    Update db.py file

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,7 +29,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
+    def _add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user with email and hashed password to the database."""
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
