52542e4e004e4e16d4187d42de8c726b75f48ca8
Amine Lamuadni <W7equwE5@gmail.com>
2024-05-31 06:54:37 +0100

    Update auth.py file

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -126,3 +126,16 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        Invalidate a user session by setting their session ID to None.
+
+        Args:
+            user_id (int): The ID of the user whose session is to be destroyed.
+
+        Returns:
+            None
+        """
+        # Update the user's session ID to None
+        self._db.update_user(user_id, session_id=None)
