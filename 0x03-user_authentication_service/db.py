ea21eef4abb90957fcb4285862bdf4cad9f3da1c
Amine Lamuadni <W7equwE5@gmail.com>
2024-05-31 06:07:54 +0100

    Update db.py file

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
 
 from user import Base, User
 
@@ -49,3 +50,29 @@ class DB:
             self._session.rollback()
             new_user = None
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Find and return the first user matching the provided search criteria.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments corresponding to the User
+                      model's attributes.
+
+        Returns:
+            User: The first User object that matches the criteria.
+
+        Raises:
+            NoResultFound: If no results are found that match the criteria.
+            InvalidRequestError: If the query arguments are incorrect or
+                                 invalid.
+        """
+        for key in kwargs:
+            if not hasattr(User, key):
+                raise InvalidRequestError
+
+        result = self._session.query(User).filter_by(**kwargs).first()
+        if result is None:
+            raise NoResultFound
+
+        return result
