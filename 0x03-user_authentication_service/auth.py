aeb098ac3588c5b6187912a0e2116d7c7457a577
Amine Lamuadni <W7equwE5@gmail.com>
2024-05-31 06:39:18 +0100

    Update auth.py file

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,10 @@ Authentication module for handling password security.
 """
 
 import bcrypt
+from db import DB
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -17,3 +21,35 @@ def _hash_password(password: str) -> bytes:
         bytes: The salted and hashed password as a byte string.
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Registers a new user in the database.
+
+        Args:
+            email (str): The email of the user to register.
+            password (str): The user's password.
+
+        Returns:
+            User: The created User object.
+
+        Raises:
+            ValueError: If a user with the given email already exists.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
+
+        raise ValueError(f"User {email} already exists")
